374720f4de674eeba8d745d33665378886eb38cc
Yuki Mukasa <mukasa@globalnet-ex.com>
2015-02-18 05:32:34 +0900

    fix docker args

--- deploy/master.sh
+++ deploy/master.sh
@@ -2,6 +2,7 @@
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_ID -p $DOCKER_PASS
 docker push argvc/nsd
-ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker stop argvc/nsd 2 > /dev/null || true
+ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker stop nsd 2 > /dev/null || true
+ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker rm nsd 2 > /dev/null || true
 ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker pull argvc/nsd
-ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker run -d -p 53:53 argvc/nsd nsd 
+ssh $DEPLOY_HOST -i ~/.ssh/id_deploy -l $DEPLOY_USER docker run -d --net=host --name nsd -p 53:53 argvc/nsd nsd
