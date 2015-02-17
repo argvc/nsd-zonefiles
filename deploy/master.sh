dadf51a7eacb8873dcb7ce78c62daf02745146c0
Yuki Mukasa <mukasa@globalnet-ex.com>
2015-02-18 03:53:00 +0900

    Change deploy script

--- deploy/master.sh
+++ deploy/master.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker login -u $DOCKER_HOST -p $DOCKER_PASS
+docker login -e $DOCKER_EMAIL -u $DOCKER_HOST -p $DOCKER_PASS
 docker push argvc/nsd
-ssh $DEPLOY_HOST docker stop argvc/nsd -i ~/.ssh/deploy -l $DEPLOY_USER
-ssh $DEPLOY_HOST docker pull argvc/nsd -i ~/.ssh/deploy -l $DEPLOY_USER
-ssh $DEPLOY_HOST docker run argvc/nsd -i ~/.ssh/deploy -l $DEPLOY_USER
+ssh $DEPLOY_HOST -i ~/.ssh/deploy -l $DEPLOY_USER docker stop argvc/nsd
+ssh $DEPLOY_HOST -i ~/.ssh/deploy -l $DEPLOY_USER docker pull argvc/nsd
+ssh $DEPLOY_HOST -i ~/.ssh/deploy -l $DEPLOY_USER docker run argvc/nsd 
